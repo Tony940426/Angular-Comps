d0b686ba4b76808674cda6fa162480a29c0a8224
Tony <tony@DESKTOP-C5TKN10.localdomain>
2023-08-21 20:28:56 +1000

    First set up of lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ElementsModule } from './elements/elements.module';
 import { CollectionsModule } from './collections/collections.module';
 import { ModsModule } from './mods/mods.module';
 import { HomeComponent } from './home/home.component';
@@ -18,7 +17,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    ElementsModule,
     CollectionsModule,
     ModsModule,
     AppRoutingModule
