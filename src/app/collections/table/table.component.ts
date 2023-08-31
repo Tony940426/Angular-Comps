6ac78cf161725969aa01cbe365f5eeccbb55d7c8
Tony Wang <Tony94042@gmail.com>
2023-08-31 20:30:11 +1000

    Started filling information into the table

--- src/app/collections/table/table.component.ts
+++ src/app/collections/table/table.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit{
-@Input() data =[];
-@Input () headers = [];
+@Input() data = [];
+@Input() headers = [];
 
 constructor(){}
 ngOnInit(): void {
