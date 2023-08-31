fc7ce7bdb56966d91f77e82f383bfebd00fc4db1
Tony Wang <Tony94042@gmail.com>
2023-08-31 20:23:17 +1000

    Started to pass data from parent to table component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
   styleUrls: ['./collections-home.component.css']
 })
-export class CollectionsHomeComponent {
+export class CollectionsHomeComponent implements OnInit {
+  data = [
+    {name: 'James', age: 24, job: 'Designed'},
+    {name: 'Jill', age: 26, job: 'Engineer'},
+    {name: 'Bob', age: 22, job: 'UX Designer'},
+  ];
+  headers = [
+    {key: 'name', label: 'name'},
+    {key: 'age', label: 'age'},
+    {key: 'job', label: 'job'}
+  ]
 
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
 }
