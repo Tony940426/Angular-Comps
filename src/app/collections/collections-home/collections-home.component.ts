1fa22a4d21bbb2c6c0c45ec4a533d641d09c83a1
Tony Wang <Tony94042@gmail.com>
2023-09-05 20:20:06 +1000

    Fixed the issue with passing the table data from parent to child

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,24 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+type Header = {
+  key: string;
+  label: string;
+};
+
+type Person = {
+  name: string;
+  age: number;
+  job: string;
+};
+
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
   styleUrls: ['./collections-home.component.css']
 })
+
 export class CollectionsHomeComponent implements OnInit {
-  data = [
+  data: Person[] = [
     {name: 'James', age: 24, job: 'Designed'},
     {name: 'Jill', age: 26, job: 'Engineer'},
-    {name: 'Bob', age: 22, job: 'UX Designer'},
+    {name: 'Bob', age: 22, job: 'UX Designer'}
   ];
-  headers = [
+
+  headers: Header[] = [
     {key: 'name', label: 'name'},
     {key: 'age', label: 'age'},
     {key: 'job', label: 'job'}
-  ]
+  ];
 
   constructor(){}
-  ngOnInit(): void {
-    
-  }
+
+  ngOnInit(): void {}
 }
--- src/app/collections/table/table.component.ts
+++ src/app/collections/table/table.component.ts
@@ -5,12 +5,11 @@ import { Component, OnInit, Input } from '@angular/core';
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
-export class TableComponent implements OnInit{
-@Input() data = [];
-@Input() headers = [];
+export class TableComponent implements OnInit {
+  @Input() data: { name: string; age: number; job: string; }[] = [];
+  @Input() headers: { key: string; label: string; }[] = [];
 
 constructor(){}
-ngOnInit(): void {
-  
-}
+
+ngOnInit(): void {}
 }
